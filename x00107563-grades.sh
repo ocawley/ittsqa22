e06c1c832611662f0ccd55cc1559be2b2dcd6423
X00107563 <collegeitt2103@gmail.com>
2014-10-16 10:54:00 +0100

    Who me??

---
+++ x00107563-grades.sh
@@ -0,0 +1,41 @@
+#Test Case 1
+#Inputs:40 40
+#Expected Outputs:Fail
+#Actual Outputs: Fail
+
+java grades 40 40
+
+#Test Case 2
+#Inputs: 39 50
+#Expected Outputs: Component fail
+#Actual Outputs: Component fail
+
+java grades 39 50
+
+#Test Case 3
+#Inputs: 50 39
+#Expected Outputs: Component fail
+#Actual Outputs: Component fail
+
+java grades 50 39
+
+#Test Case 4
+#Inputs:70 70 
+#Outputs:Pass
+#Actual Outputs:Pass
+
+java grades 70 70
+
+#Test Case 5
+#Inputs: 90 90
+#Outputs: Pass with distinction
+#Actual Outputs: Pass
+
+java grades 80 80
+
+#Test Case 6
+#Inputs: 200 101
+#Outputs: Invalid input
+#Actual Outputs: Invalid input
+
+java grades 200 101
