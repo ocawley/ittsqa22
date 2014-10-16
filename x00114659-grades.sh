daf8abd27d97a51c75e25a1b2320920d34950ef0
George <x00114659@gmail.com>
2014-10-16 10:58:02 +0100

    fjaads;lfjdsa;klfjads;fsdj;flsadjf;lsaj;

---
+++ x00114659-grades.sh
@@ -0,0 +1,35 @@
+# Test Case 1
+# Inputs: 80 80
+# Expected Output: Pass With Distinction
+# Actual Output: Pass
+java grades 80 80
+
+# Test Case 2
+# Inputs: 60 60
+# Expected Output: PASS
+# Actual Output: PASS
+java grades 60 60
+
+# Test Case 3
+# Inputs: 100 70
+# Expected Output: Invalid Input, Component Fail / ENHANCE
+# Actual Output: Invalid Input
+java grades 101 70
+
+# Test Case 4
+# Inputs: 100 39
+# Expected Output: Component Fail
+# Actual Output: Component Fail
+java grades 100 39
+
+# Test Case 5
+# Inputs: 0 0
+# Expected Output: Fail 
+# Actual Output: Component Fail
+java grades 0 0
+
+# Test Case 6
+# Inputs: 100 100
+# Expected Output: Pass With Distinction
+# Actual Output: Pass With Distinction
+java grades 100 100
