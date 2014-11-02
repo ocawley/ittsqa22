e050ec2c5fec1f83b98e8baf9bbf6c5064d8f22c
X00115013 <X00115013.gmail.com>
2014-11-02 16:26:21 +0000

    need this to work

---
+++ grades_v2.sh
@@ -0,0 +1,35 @@
+#test Case 1
+#Inputs:120 40
+#Expected Outputs: Invalid input
+#Actual Outputs: Invalid input
+java grades 120 40
+#Test Case 2
+#Inputs: 50 120
+#Expected Outputs: Invalid input
+#Actual Outputs: Invalid input
+java grades 50 120
+#Test Case 3
+#Inputs: 38 50
+#Expected Outputs: Component fail
+#Actual Outputs: Component fail
+java grades 38 50
+#Test Case 4
+#Inputs:50 38
+#Outputs:Component fail
+#Actual Outputs:Component fail
+java grades  50 38
+#Test Case 5
+#Inputs: 50
+#Outputs: Fail
+#Actual Outputs: Fail
+java grades 50 
+#Test Case 6
+#Inputs: 65 70
+#Outputs: Pass
+#Actual Outputs: Pass
+java grades 65 70
+#Test Case 7
+#Inputs: 85 95
+#Outputs: Pass with distinction
+#Actual Outputs: Pass with distinction
+java grades 85 95
